f0830de43fbb8ffe7834671a47c10acc03cf9a31
zexho <zouzhihao994@gmail.com>
2022-03-20 20:28:46 +0800

    feat(namesrv): 添加启动成功注释

--- namesrv/src/main/java/remote/NameSrvRemoting.java
+++ namesrv/src/main/java/remote/NameSrvRemoting.java
@@ -1,6 +1,7 @@
 package remote;
 
 import common.Host;
+import lombok.extern.log4j.Log4j2;
 import netty.server.NettyRemotingServerImpl;
 import netty.server.NettyServerConfig;
 
@@ -8,6 +9,7 @@ import netty.server.NettyServerConfig;
  * @author Zexho
  * @date 2021/12/15 3:57 PM
  */
+@Log4j2
 public class NameSrvRemoting {
 
     private final NettyRemotingServerImpl remotingServer;
@@ -21,6 +23,7 @@ public class NameSrvRemoting {
 
     public void start() {
         this.remotingServer.start();
+        log.info("namesrv started successfully, listening to port 9876");
     }
 
 }
